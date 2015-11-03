cb28dbb792d7f46ab07cfaaa7cdd3357a4ecf6b2
Charles Paul <cpaul@ansible.com>
2015-11-03 22:27:56 +0900

    adding docstring

--- cloud/vmware/vmware_vm.py
+++ cloud/vmware/vmware_vm.py
@@ -1,6 +1,118 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+DOCUMENTATION = '''
+---
+module: vmware_vm
+short_description: Manage VMware vSphere Virtual Machines
+description:
+    - Manage VMware vSphere Virtual Machines
+version_added: 2.0
+author: "Charles Paul (@chrrrles)"
+notes:
+  - Tested on vSphere 6.0
+requirements:
+    - "python >= 2.6"
+    - PyVmomi
+options:
+    hostname:
+        description:
+            - The hostname or IP address of the vSphere vCenter API server
+        required: True
+    username:
+        description:
+            - The username of the vSphere vCenter
+        required: True
+        aliases: ['user', 'admin']
+    password:
+        description:
+            - The password of the vSphere vCenter
+        required: True
+        aliases: ['pass', 'pwd']
+    vm_name:
+        description:
+            - The name of the VM.
+        required: True
+    vm_datacenter:
+        description:
+            - The name of the datacenter for the VM.
+        required: False
+    vm_pool:
+        description:
+            - The name of the resource pool for the VM.
+        required: False
+    vm_cluster:
+        description:
+            - The name of the cluster for the VM.
+        required: False
+    vm_datastore:
+        description:
+            - The name of the datastore for the VM.
+        required: False
+    vm_folder:
+        description:
+            - The name of the folder for the VM.
+        required: False
+    vm_template:
+        description:
+            - The name of the template used for the VM.
+        required: False
+    vm_power:
+        description:
+            - The power on state for the VM
+        default: "yes"
+        choices: ["yes", "no"]
+    vm_memoryMB:
+        description:
+            - Integer for the amount of memory (in MB) to provision for the VM
+        required: False
+    vm_numCPUs:
+        description:
+            - Integer for the number of CPUs to provision for the VM
+        required: False
+    vm_guest_id:
+        description:
+            - The guestId string to use for the VM
+        default: "otherLinuxGuest"
+    vm_nic_type:
+        description:
+            - The network card type to use for the VM
+        choices: ['vmxnet', 'vmxnet3', 'e1000', 'e1000e']
+    vm_network
+        description:
+            - The name of the network to use for the VM
+        default: "VM Network"
+    state:
+        description:
+            - If the datacenter should be present or absent
+        choices: ['present', 'absent']
+        required: True
+'''
+
+EXAMPLES = '''
+- name: Destroy VM
+      local_action: >
+        vmware_vm
+        hostname=10.1.1.2
+        username=root 
+        password=vmware
+        vm_name=test-vm
+- name: Create VM
+      local_action: >
+        vmware_vm
+        hostname=10.1.1.2
+        username=root 
+        password=vmware
+        vm_name=test-vm
+        vm_datacenter=Datacenter1
+        vm_datastore=test-volume
+        vm_pool=Testing
+        vm_name=test-vm
+        vm_memoryMB=2048
+        vm_numCPUs=2
+        vm_power=false
+'''
+
 try:
   from pyVmomi import vim, vmodl
   HAS_PYVMOMI = True
@@ -16,10 +128,10 @@ def get_obj(content, vimtype, name=None):
 
 def create_vm(module):
   content = module.params['content']
-  vm_dc = module.params['vm_dc']
+  vm_datacenter = module.params['vm_datacenter']
   vm_name = module.params['vm_name']
   vm_folder = module.params['vm_folder']
-  ds_path = "[%s] %s" % (module.params['vm_ds'], vm_name)
+  ds_path = "[%s] %s" % (module.params['vm_datastore'], vm_name)
   cluster = module.params['vm_cluster'] 
   vm_pool = module.params['vm_pool']
 
@@ -71,20 +183,20 @@ def create_vm(module):
 def clone_vm(module):
   content = module.params['content']
   template = get_obj(content, [vim.VirtualMachine], module.params['vm_template'])
-  vm_dc = module.params['vm_dc']
+  vm_datacenter = module.params['vm_datacenter']
   vm_pool = module.params['vm_pool']
   vm_cluster = module.params['vm_cluster']
-  if module.params['vm_ds']:
-    vm_ds = get_obj(content, [vim.Datastore], module.params['vm_ds'])
+  if module.params['vm_datastore']:
+    vm_datastore = get_obj(content, [vim.Datastore], module.params['vm_datastore'])
   else:
-    vm_ds = get_obj(content, [vim.Datastore], template.datastore[0].info.name)
+    vm_datastore = get_obj(content, [vim.Datastore], template.datastore[0].info.name)
   vm_folder = module.params['vm_folder']
   vm_name = module.params['vm_name']
   vm_spec = module.params['vm_spec']
 
   # set our relospec
   relospec = vim.vm.RelocateSpec()
-  relospec.datastore = vm_ds
+  relospec.datastore = vm_datastore
   relospec.pool = vm_pool
 
   clonespec = vim.vm.CloneSpec()
@@ -121,7 +233,7 @@ def state_create(module):
     module.exit_json(True, None)
   content = module.params['content']
   vm_cluster = module.params['vm_cluster']
-  module.params['vm_dc'] = get_obj(content, [vim.Datacenter], module.params['vm_dc'])
+  module.params['vm_datacenter'] = get_obj(content, [vim.Datacenter], module.params['vm_datacenter'])
   module.params['vm_cluster'] = get_obj(content, [vim.ClusterComputeResource], vm_cluster)
   if module.params['vm_pool']:
     module.params['vm_pool'] = get_obj(content, [vim.ResourcePool], module.params['vm_pool'])
@@ -130,7 +242,7 @@ def state_create(module):
   if module.params['vm_folder']:
     module.params['vm_folder'] = get_obj(content, [vim.Folder], module.params['vm_folder'])
   else:
-    module.params['vm_folder'] = module.params['vm_dc'].vmFolder
+    module.params['vm_folder'] = module.params['vm_datacenter'].vmFolder
 
   if module.params['vm_template']:
     return clone_vm(module)
@@ -161,20 +273,20 @@ def state_exit(module):
 def main():
   argument_spec = vmware_argument_spec()
   argument_spec.update(dict(
-    state = dict(default='present', choices=['present', 'absent', 'stopped', 'maintenance'], type='str'),
-    force = dict(default=False, type='bool'),
-    vm_dc = dict(default=None, type='str'),
+    # [XXX] should be this: state = dict(default='present', choices=['present', 'absent', 'stopped', 'maintenance'], type='str'),
+    state = dict(default='present', choices=['present', 'absent'], type='str'),
+    vm_name = dict(required=True, type='str'), 
+    vm_datacenter = dict(default=None, type='str'),
     vm_pool = dict(default=None, type='str'),
     vm_cluster = dict(default=None, type='str'),
-    vm_ds = dict(default=None, type='str'),
+    vm_datastore = dict(default=None, type='str'),
     vm_folder = dict(default=None, type='str'),
     vm_template = dict(default=None, type='str'),
-    vm_name = dict(required=True, type='str'), 
     vm_power = dict(default=True, type='bool'),
     vm_memoryMB = dict(default=None, type='int'),
     vm_numCPUs = dict(default=None, type='int'),
     vm_guest_id = dict(default='otherLinuxGuest', type='str'),
-    vm_nic_type = dict(default='vmxnet3', type='str'),
+    vm_nic_type = dict(default='vmxnet3', choices=['vmxnet', 'vmxnet3', 'e1000', 'e1000e'], type='str'),
     vm_network = dict(default="VM Network", type='str')
     ))
   
